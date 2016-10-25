5ccfa1bbcbd4c025cd4961ef2547cbb24cfadc0e
mohayonao <mohayonao@gmail.com>
2016-10-25 21:03:14 +0900

    change api LEDPad

--- src/client/components/LEDButton.js
+++ src/client/components/LEDButton.js
@@ -1,3 +1,3 @@
-import LEDPadRect from "./ui/LEDPadRect";
+import LEDPad from "./ui/LEDPad";
 
-export default LEDPadRect({ size: 16, rx: 4, ry: 4 });
+export default LEDPad("rect", { size: 16, rx: 4, ry: 4 });
--- src/client/components/ui/LEDPad.js
+++ src/client/components/ui/LEDPad.js
@@ -1,49 +1,116 @@
-import React, { PropTypes } from "react";
+import React, { Component, PropTypes } from "react";
 import UIComponent from "./UIComponent";
 
-export default function LEDPad(computeStyles, styles) {
-  return (Component) => {
-    return @UIComponent() class LEDPad extends React.Component {
-      static propTypes = {
-        onValueChange: PropTypes.func.isRequired,
-      };
-      static defaultProps = {
-        onValueChange: () => {},
-      };
+const shapes = {};
 
-      constructor(...args) {
-        super(...args);
-        this._styles = computeStyles(this.props, styles);
-        this._onmouse = false;
-      }
+export default function LEDPad(shape, styles) {
+  const delegate = shapes[shape];
 
-      onMouseDown() {
-        this._onmouse = true;
-        this.props.onValueChange(0x7f);
-      }
+  return @UIComponent() class LEDPadComponent extends Component {
+    static propTypes = {
+      data         : PropTypes.number.isRequired,
+      onValueChange: PropTypes.func  .isRequired,
+    };
+    static defaultProps = {
+      onValueChange: () => {},
+    };
 
-      onMouseUp() {
-        this._onmouse = false;
-        this.props.onValueChange(0x00);
-      }
+    constructor(...args) {
+      super(...args);
+      this._styles = this::delegate.computeStyles(this.props, styles);
+      this._onmouse = false;
+    }
 
-      onMouseOut() {
-        if (this._onmouse) {
-          this.onMouseUp();
-        }
-      }
+    onMouseDown() {
+      this._onmouse = true;
+      this.props.onValueChange(0x7f);
+    }
 
-      onTouchStart() {
-        this.onMouseDown();
-      }
+    onMouseUp() {
+      this._onmouse = false;
+      this.props.onValueChange(0x00);
+    }
 
-      onTouchEnd() {
+    onMouseOut() {
+      if (this._onmouse) {
         this.onMouseUp();
       }
+    }
 
-      render() {
-        return (<Component { ...this.props } styles={ this._styles }/>);
-      }
+    onTouchStart() {
+      this.onMouseDown();
+    }
+
+    onTouchEnd() {
+      this.onMouseUp();
+    }
+
+    render() {
+      return this::delegate.render(this.props.data, this._styles);
     }
   };
 }
+
+shapes["rect"] = {
+  computeStyles(props, styles) {
+    const size = styles.size;
+    const x = typeof props.x === "number" ? props.x : props.cx - size / 2;
+    const y = typeof props.y === "number" ? props.y : props.cy - size / 2;
+    const width  = size;
+    const height = size;
+
+    return {
+      rx: 10,
+      ry: 10,
+      stroke: "#050504",
+      strokeWidth: 1,
+      ...styles,
+      x, y, width, height,
+    };
+  },
+  render(data, styles) {
+    const fill = `url(#led${ data })`;
+
+    return (<rect { ...styles } fill={ fill }/>);
+  }
+};
+
+shapes["oval"] = {
+  computeStyles(props, styles) {
+    const r = styles.size / 2;
+    const cx = typeof props.cx === "number" ? props.cx : props.x + r;
+    const cy = typeof props.cy === "number" ? props.cy : props.y + r;
+
+    return {
+      stroke: "#050504",
+      strokeWidth: 1,
+      ...styles,
+      cx, cy, r,
+    };
+  },
+  render(data, styles) {
+    const fill = `url(#led${ data })`;
+
+    return (<circle { ...styles } fill={ fill }/>);
+  }
+};
+
+shapes["ring"] = {
+  computeStyles(props, styles) {
+    const r = styles.size / 2;
+    const cx = typeof props.cx === "number" ? props.cx : props.x + r;
+    const cy = typeof props.cy === "number" ? props.cy : props.y + r;
+
+    return {
+      fill: "transparent",
+      strokeWidth: 8,
+      ...styles,
+      cx, cy, r,
+    };
+  },
+  render(data, styles) {
+    const stroke = `url(#led${ data })`;
+
+    return (<circle { ...styles } stroke={ stroke }/>);
+  }
+};
